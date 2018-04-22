8805ed5f9ca75be7abf783debfe2e621a988fba4
jirina-trojankova <jirina.trojankova@seznam.cz>
2018-04-22 21:42:44 +0200

    Functions drenchRight and drenchBottom

--- drench.js
+++ drench.js
@@ -20,9 +20,9 @@ var square = document.getElementById(id);
 var right = document.getElementById(idRight);
 var bottom = document.getElementById(idBottom);
 
-//adds class drench to the first same colors
-addDrench();
 
+drenchRight();
+drenchDown();
 
 //randomly chooses number
 function getNumber() {
@@ -48,9 +48,8 @@ DOMTokenList.prototype.removeMany = function (classes) {
 
 
 
-
-//if colors are the same, this adds class drench
-function addDrench() {
+//PRIDA CLASS VSEM CTVERCUM STEJNE BARVY SMEREM VPRAVO
+function drenchRight() {
   id = 1;
   idRight = 2;
   var square = document.getElementById(id);
@@ -58,9 +57,6 @@ function addDrench() {
 
   var right = document.getElementById(idRight);
   right = (right.classList);
-
-  var bottom = document.getElementById(idBottom);
-  bottom = (bottom.classList);
 
   while (JSON.stringify(square) === JSON.stringify(right)) {
     square = document.getElementById(id);
@@ -73,88 +69,99 @@ function addDrench() {
     square = (square.classList);
     right = (right.classList);
   }
+  square = document.getElementById(id);
+  square.className += " drench";
+}
 
-  console.log(square);
-  console.log(bottom);
+//PRIDA CLAS VSEM CTVERCUM STEJNE BARVY SMEREM DOLU
+function drenchDown() {
+  id = 1;
+  idBottom = 11;
+  var square = document.getElementById(id);
+  square = (square.classList);
+
+  var bottom = document.getElementById(idBottom);
+  bottom = (bottom.classList);
+
   if (square.contains('drench') == true) {
     square.remove('drench');
   }
+
   while (JSON.stringify(square) === JSON.stringify(bottom)) {
     square = document.getElementById(id);
     square.className += " drench";
-    
-      id = idBottom;
-      idBottom = idBottom + chessboardSize;
-      square = document.getElementById(id);
-      bottom = document.getElementById(idBottom);
-      console.log(square);
-      console.log(bottom);
 
-      square = (square.classList);
-      bottom = (bottom.classList);
-    }
+    id = idBottom;
+    idBottom = idBottom + chessboardSize;
     square = document.getElementById(id);
-    square.className += " drench";
+    bottom = document.getElementById(idBottom);
+
+    square = (square.classList);
+    bottom = (bottom.classList);
+  }
+  square = document.getElementById(id);
+  square.className += " drench";
+}
+
+
+
+
+//CHANGES FIRST COLOR on click and adds class drench to the same colors
+function changeColor(newColor) {
+  //in the first square changes color (class)
+  id = 1;
+  idRight = 2;
+  var square = document.getElementById(id);
+  square.classList.removeMany('color1 color2 color3 color4 color5 color6');
+  square.classList.add(newColor);
+  //check if ther's class drench at right
+  var right = document.getElementById(idRight);
+
+  //if there's class drench give it new color
+  while (right.classList.contains('drench') == true) {
+
+    right.classList.removeMany('color1 color2 color3 color4 color5 color6');
+    right.classList.add(newColor);
+    //move step right and do the same
+
+    idRight++;
+    right = document.getElementById(idRight);
   }
 
+  //adds class drench to the next group of color
+  idRight++;
+  id = idRight - 1;
 
+  square = document.getElementById(id);
+  right = document.getElementById(idRight);
 
-  //CHANGES FIRST COLOR on click and adds class drench to the same colors
-  function changeColor(newColor) {
-    //in the first square changes color (class)
-    id = 1;
-    idRight = 2;
-    var square = document.getElementById(id);
-    square.classList.removeMany('color1 color2 color3 color4 color5 color6');
-    square.classList.add(newColor);
-    //check if ther's class drench at right
-    var right = document.getElementById(idRight);
+  square = (square.classList);
+  right = (right.classList);
 
-    //if there's class drench give it new color
-    while (right.classList.contains('drench') == true) {
+  while (JSON.stringify(square) === JSON.stringify(right)) {
+    square = document.getElementById(id);
+    square.className += " drench";
 
-      right.classList.removeMany('color1 color2 color3 color4 color5 color6');
-      right.classList.add(newColor);
-      //move step right and do the same
-
-      idRight++;
-      right = document.getElementById(idRight);
-    }
-
-    //adds class drench to the next group of color
+    id++;
     idRight++;
-    id = idRight - 1;
 
     square = document.getElementById(id);
     right = document.getElementById(idRight);
 
     square = (square.classList);
     right = (right.classList);
-
-    while (JSON.stringify(square) === JSON.stringify(right)) {
-      square = document.getElementById(id);
-      square.className += " drench";
-
-      id++;
-      idRight++;
-
-      square = document.getElementById(id);
-      right = document.getElementById(idRight);
-
-      square = (square.classList);
-      right = (right.classList);
-    }
-    square = document.getElementById(id);
-    square.className += " drench";
   }
+  square = document.getElementById(id);
+  square.className += " drench";
+}
 
-  //BUG - square changes color even if it doesn't match
+//BUG - square changes color even if it doesn't match
 
-  //TO ADD drenching also from top to BOTTOM, also to the LEFT and UP
-  //add counter
-
-
+//TO ADD drenching also from top to BOTTOM, also to the LEFT and UP
+//add counter
 
 
-  /*if(square.classList.contains('drench') !=true) {
-    square.className += " drench";*/
+
+
+/*if(square.classList.contains('drench') !=true) {
+  square.className += " drench";*/
